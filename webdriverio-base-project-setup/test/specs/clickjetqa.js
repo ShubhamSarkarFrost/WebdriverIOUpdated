bd7a1184a0a4ca2e0627317f645e4128b627a653
Shubham Sarkar <49359694+ShubhamSarkarFrost@users.noreply.github.com>
2023-08-23 10:22:33 +0530

    UPDATED_WEBDRIVERIO_CODE_23_08_2023

--- webdriverio-base-project-setup/test/specs/clickjetqa.js
+++ webdriverio-base-project-setup/test/specs/clickjetqa.js
@@ -9,31 +9,39 @@ describe('Home Page', () => {
   });
 }); 
 
-describe('Home Page', () => {
+describe('QAClickJet - Assert the color of the text when checkbox with same label is clicked ', () => {
   it('Open URL & assert title', async () => {
     // Open URL
     await browser.url("https://rahulshettyacademy.com/dropdownsPractise/");
     await browser.maximizeWindow();
     
-    const roundTrip = ".//input[@id='ctl00_mainContent_rbtnl_Trip_1']";
-    const roundTripTagColor = ".//label[@for='ctl00_mainContent_rbtnl_Trip_1']"
-    await $(roundTrip).click();
-    const color = await $(roundTripTagColor).getCSSProperty('color')
+    const roundTrip = await $('//input[@id="ctl00_mainContent_rbtnl_Trip_1"]');
+    const roundTripTagColor = await $('//label[@for="ctl00_mainContent_rbtnl_Trip_1"]');
+    await roundTrip.click()
+    const color = await roundTripTagColor.getCSSProperty('color')
     console.log(color.value)
 
   });
 }); 
 
 
-describe('Home Page', () => {
+describe('QAClickJet - Select From and To Destination from FROM & TO Dropdown', () => {
   it('Open URL & assert title', async () => {
     // Open URL
     await browser.url("https://rahulshettyacademy.com/dropdownsPractise/");
     await browser.maximizeWindow();
     
-    const FromDestination = ".//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']"
-    await $(FromDestination).click();
-    
-
-  });
+    const FromDestination = await $('//input[@id="ctl00_mainContent_ddl_originStation1_CTXT"]')
+    const ToDestination = await $('//*[@id="ctl00_mainContent_ddl_destinationStation1_CTNR"]//a[@value="RDP"]')
+    await FromDestination.click();
+    await FromDestination.setValue('MAA');
+    await ToDestination.waitForClickable({ timeout: 3000 });
+    await ToDestination.click()
+    const actualToDestinationValue = ToDestination.getValue();
+    console.log(actualToDestinationValue)
+    const actualFromDestinationValue = FromDestination.getValue();
+    console.log(actualFromDestinationValue)
+    await expect(FromDestination).toHaveText('Chennai (MAA)');
+    await expect(ToDestination).toHaveText('Durgapur (RDP)');
+    });
 }); 
