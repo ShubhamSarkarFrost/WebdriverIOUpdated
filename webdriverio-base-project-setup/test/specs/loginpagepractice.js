f4592f57b4e7c18821b140072cd219eb242d3ea3
Shubham Sarkar <49359694+ShubhamSarkarFrost@users.noreply.github.com>
2023-09-05 03:30:22 -0700

    WEBDRIVER_IO_05_09_2023_04:00_PM_IST

--- webdriverio-base-project-setup/test/specs/loginpagepractice.js
+++ webdriverio-base-project-setup/test/specs/loginpagepractice.js
@@ -2,7 +2,10 @@ describe('Practice Login Page', () =>{
     it('Login Fail Page', async() =>{
         await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
         console.log( await browser.getTitle());
-        await expect(browser).toHaveTitleContaining('loginpagePractise')
+        await expect(browser).toHaveTitleContaining('loginpagePractise');
+        await $("//input[@id='username']").addValue("rahulshettyacademy");
+        await $("#password").setValue("datanotpossible");
+        
         
     })
 } )
